aab6d8d69cd588d79c95ec863210f371fa951fd6
Aletheaaa1 <1120495490@qq.com>
2023-10-16 19:21:18 +0800

    Geometry WireFrame

---
+++ Shaders/Wireframe/wireframe.vs
@@ -0,0 +1,19 @@
+#version 460
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec3 FragPos;
+out vec3 Normal;
+
+void main()
+{
+	mat3 NormalMatrix = transpose(inverse(mat3(model)));
+	Normal =normalize( NormalMatrix * aNormal);
+	FragPos = (model * vec4(aPos, 1.0)).xyz;
+
+	gl_Position = model * vec4(aPos, 1.0);
+}
---
+++ Source/WireframeWithGeometry.cpp
@@ -0,0 +1,203 @@
+/*
+	在着色表面上生成线框结构;
+	当片元距离三角形边缘一定距离时，它将被着色并与边缘颜色混合。否则，片元将被正常着色。
+*/
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include <random>
+#include <memory>
+
+#include "../Headers/Model.h"
+#include "../Headers/Shader.h"
+#include "../Headers/Texture.h"
+#include "../Headers/Camera.h"
+#include "../Headers/VertexArrayObject.h"
+#include "../Headers/VertexBufferObject.h"
+
+Camera camera{ glm::vec3(0, 0.0f, 2.0f), 0.0f, 0.0f, glm::vec3(0, 1.0f, 0) };
+#pragma region Interaction
+float x_last, y_last;
+bool first_mouse = true;
+void MouseCallback(GLFWwindow* window, double x_pos, double y_pos)
+{
+	if (first_mouse == true)
+	{
+		x_last = x_pos;
+		y_last = y_pos;
+		first_mouse = false;
+	}
+
+	float x_offset = x_pos - x_last;
+	float y_offset = y_pos - y_last;
+
+	x_last = x_pos;
+	y_last = y_pos;
+
+	camera.UpdateCameraView(x_offset, y_offset);
+	camera.GetViewMatrix();
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera.speedZ = -0.03f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.speedZ = 0.03f;
+	}
+	else
+	{
+		camera.speedZ = 0.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.speedX = -0.03f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.speedX = 0.03f;
+	}
+	else
+	{
+		camera.speedX = 0.0f;
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+	{
+		camera.speedY = 0.03f;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+	{
+		camera.speedY = -0.03f;
+	}
+	else
+	{
+		camera.speedY = 0.0f;
+	}
+}
+#pragma endregion Interaction
+
+void RenderQuad();
+
+int main()
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	glfwWindowHint(GLFW_SAMPLES, 16);
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL())
+	{
+		std::cout << "glad loading error \n";
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, false);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, MouseCallback);
+
+#pragma region Configue
+	std::string shader_dir = "./Shaders/Wireframe/";
+	Shader shader{ shader_dir + "wireframe.vs", shader_dir + "wireframe.fs", shader_dir + "wireframe.gs" };
+	//Shader shader{ shader_dir + "wireframe.vs", shader_dir + "wireframe.fs" };
+	Texture texture{ "./Textures/box.png" };
+	Model object{ "./Models/bs_ears.obj" };
+
+	glm::vec3 light_pos = glm::vec3(0.0f, 20.0f, 0.0f);
+	glm::vec3 light_color = glm::vec3(1.0f, 1.0f, 1.0f);
+#pragma endregion Configue
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_MULTISAMPLE);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.2f, 0.2f, 0.2f, 0.2f);
+		ProcessInput(window);
+		camera.UpdateCameraPosition();
+
+#pragma region Render
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(800) / static_cast<float>(600), 0.001f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+		//model = glm::scale(model, glm::vec3(0.1f));
+		//model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		texture.Usetexture(0);
+		texture.Bind();
+
+		shader.Bind();
+		shader.SetUniformMat4("model", model);
+		shader.SetUniformMat4("view", view);
+		shader.SetUniformMat4("projection", projection);
+		shader.SetUniform1f("halfSize", 0.15f);
+		shader.SetUniform3v("lightColor", light_color);
+		shader.SetUniform3v("lightPos", light_pos);
+		shader.SetUniform3v("cameraPos", camera.position);
+		shader.SetUniform1f("lineWidth", 0.01f);
+		shader.SetUniform3v("lineColor", glm::vec3(0.1f, 0.0f, 0.1f));
+
+		object.Draw(shader);
+#pragma endregion Render
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void RenderQuad()
+{
+	float quad_vertice[] = {
+		// positions						  // texture Coords
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+		 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		 1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+	};
+
+	VertexBufferObject vbo{ quad_vertice, sizeof(quad_vertice) };
+	vbo.Bind();
+
+	VertexArrayObject vao{};
+	vao.AddBuffer(0, 3, GL_FLOAT, false, 8 * sizeof(float), 0);
+	vao.AddBuffer(1, 3, GL_FLOAT, false, 8 * sizeof(float), 3 * sizeof(float));
+	vao.AddBuffer(2, 2, GL_FLOAT, false, 8 * sizeof(float), 6 * sizeof(float));
+	vao.Bind();
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
